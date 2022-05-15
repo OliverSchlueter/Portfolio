5c5ef05e53dac2cc16131d3ba90aade812710797
OliverSchlueter <oliverhd1303@gmail.com>
2022-05-15 19:39:29 +0200

    Changed discord-message to mail

--- assets/php/contactManager.php
+++ assets/php/contactManager.php
@@ -1,4 +1,12 @@
 <?php
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+    
+    require 'libs/PHPMailer/src/PHPMailer.php';
+    require 'libs/PHPMailer/src/SMTP.php';
+    require 'libs/PHPMailer/src/Exception.php';
 
 if(isset($_POST["submit"])){
     $subject = $_POST["subject"];
@@ -6,51 +14,84 @@ if(isset($_POST["submit"])){
     $email = $_POST["email"];
     $message = $_POST["message"];
 
+    $timestamp = time();
+    $id = substr(hash("sha256", "Oliver Portfolio Message $subject $name $email $message $id $timestamp"), 0, 10);
 
-    // temporarily sending messages to a discord channel.
-    // TODO: move to a database and add an adminpanel later
-    sendDiscordMessage(json_encode([
-        //"content" => "Neue Nachricht vom Portfolio!",
-        "username" => "Portfolio Nachrichten",
-        "tts" => false,
+    $config = json_decode(file_get_contents("config.json"));
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = "plesk2.living-bots.net";
+        $mail->SMTPAuth = true;
+        $mail->Username = "spam@xn--schlter-oliver-jsb.de";
+        $mail->Password = $config->{"mail-password"};
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
     
-        "embeds" => [
-            [
-                "title" => "Neue Nachricht vom Portfolio!",
-                "type" => "rich",
-                "timestamp" => date("c", strtotime("now")),
-                "color" => hexdec("ff6a00"),
-                "fields" => [
-                    [
-                        "name" => "Betreff",
-                        "value" => $subject,
-                        "inline" => false
-                    ],
-                    [
-                        "name" => "Name",
-                        "value" => $name,
-                        "inline" => true
-                    ],
-                    [
-                        "name" => "Email",
-                        "value" => $email,
-                        "inline" => true
-                    ],
-                    [
-                        "name" => "Datum & Zeit",
-                        "value" => date("d.m.Y H:i:s"),
-                        "inline" => true
-                    ],
-                    [
-                        "name" => "Nachricht",
-                        "value" => $message,
-                        "inline" => false
-                    ]
-                ]
-            ]
-        ]
+        //Recipients
+        $mail->setFrom("spam@xn--schlter-oliver-jsb.de", "Portfolio");
+        $mail->addAddress("info@xn--schlter-oliver-jsb.de");
+        $mail->addCC("spam@xn--schlter-oliver-jsb.de");
     
-    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ));
+        //Content
+        $mail->isHTML(true);
+        $mail->CharSet = "UTF-8";
+        $mail->Encoding = "base64";
+        $mail->Subject = "Neue Portfolio-Nachricht (#$id)";
+        $mail->Body = "Name: $name<br>Email: $email<br>Betreff: $subject<br>Nachricht:<br>$message";
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+
+    // sendDiscordMessage(json_encode([
+    //     "username" => "Portfolio Nachrichten",
+    //     "tts" => false,
+    
+    //     "embeds" => [
+    //         [
+    //             "title" => "Neue Nachricht vom Portfolio!",
+    //             "type" => "rich",
+    //             "timestamp" => date("c", strtotime("now")),
+    //             "color" => hexdec("ff6a00"),
+    //             "fields" => [
+    //                 [
+    //                     "name" => "Betreff",
+    //                     "value" => $subject,
+    //                     "inline" => false
+    //                 ],
+    //                 [
+    //                     "name" => "Name",
+    //                     "value" => $name,
+    //                     "inline" => true
+    //                 ],
+    //                 [
+    //                     "name" => "Email",
+    //                     "value" => $email,
+    //                     "inline" => true
+    //                 ],
+    //                 [
+    //                     "name" => "Datum & Zeit",
+    //                     "value" => date("d.m.Y H:i:s"),
+    //                     "inline" => true
+    //                 ],
+    //                 [
+    //                     "name" => "Nachricht",
+    //                     "value" => $message,
+    //                     "inline" => false
+    //                 ]
+    //             ]
+    //         ]
+    //     ]
+    
+    // ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ));
 
     echo "<script> 
             window.alert('Erfolgreich Nachricht gesendet!'); 
@@ -59,7 +100,6 @@ if(isset($_POST["submit"])){
 }
 
 function sendDiscordMessage($json_data){
-    $config = json_decode(file_get_contents("config.json"));
     $webhookurl = $config->{"discord-webhook-link"};
 
     $ch = curl_init( $webhookurl );
