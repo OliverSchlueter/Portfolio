b4ad24a9ae2a6fbde027ce5f65a7fe1d3f3b7c87
OliverSchlueter <oliverhd1303@gmail.com>
2022-08-09 19:18:09 +0200

    Added basic admin login site

--- admin/login/index.php
+++ admin/login/index.php
@@ -1,23 +1,49 @@
+<?php
+    session_start();
+
+    require_once "../../assets/php/config.php";
+    $config = reloadConfig("../../assets/data/config.json");
+
+    if(isset($_POST['login'])){
+        if($config->{"admin-password"} != hash("SHA512", $_POST['login'])){
+            $_SESSION['admin'] = "";
+            die("Wrong password");
+        } else {
+            $_SESSION['admin'] = $_POST['login'];
+            header("Location: ../");
+            die();
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Oliver Schlüter | Admin</title>
+    <title>Oliver Schlüter | Admin Login</title>
     <link rel="shortcut icon" href="../../assets/img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <link rel="stylesheet" href="../../assets/css/snackbar.css">
+    <link rel="stylesheet" href="../../assets/css/form.css">
 </head>
 <body>
+    <style>
+        main{
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    </style>
     <main>
-        <h1>Adminbereich</h1>
-    </main>
+        <h1>Admin Login</h1>
+        <form method="POST">
+            <label for="login">PASSWORD</label>
+            <input type="password" name="login" placeholder="Admin password">
 
-    <div class="footer-line"></div>
-    <footer>
-        <p style="max-width: 100%;">&copy; 2022 Oliver Schlüter</p>
-        <p style="max-width: 100%; font-size: 1em;">oliver_schlueter@outlook.com &bull; <a href="https://github.com/OliverSchlueter" target="_blank">GitHub.com/OliverSchlueter</a> &bull; <a href="../../Impressum/">Impressum</a></p>
-    </footer>
-    
+            <button type="submit">Senden</button>
+        </form>
+    </main>
     <script src="../../assets/js/main.js"></script>
     <script src="../../assets/js/snackbar.js"></script>
 </body>
