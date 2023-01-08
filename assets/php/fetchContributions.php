c1e281d17a097ab7ee2c5b18723ec47373232f5c
OliverSchlueter <oliverhd.1303@gmail.com>
2023-01-08 14:05:46 +0100

    Fix contributions counter

--- assets/php/fetchContributions.php
+++ assets/php/fetchContributions.php
@@ -3,13 +3,15 @@
 
     $site = file_get_contents("https://github.com/users/OliverSchlueter/contributions");
 
-    //echo $site;
+//    echo $site;
 
     $pos = strpos($site, "contributions", 50);
 
-    $lengthOfNumber = 3; // change when amount gets > 999
+    $lengthOfNumber = 5; // change when amount gets > 999
 
-    $contributions = substr($site, $pos-10, 3);
+    $contributions = substr($site, $pos-12, $lengthOfNumber);
+
+    $contributions = str_replace(",", "", $contributions);
 
     echo json_encode([
         "contributions" => $contributions,
