aad84cef1568bcabeabd30b7900e1cb2ccba12e7
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-06 17:40:05 +0100

    Update project card style

---
+++ assets/js/card-effect.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('.cards');
+const cards = document.querySelectorAll(".card");
+
+wrapper.addEventListener('mousemove', function ($event) {
+
+    cards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+
+        const x = $event.clientX - rect.left;
+        const y = $event.clientY - rect.top;
+
+        card.style.setProperty('--xPos', `${x}px`);
+        card.style.setProperty('--yPos', `${y}px`);
+    });
+
+});
